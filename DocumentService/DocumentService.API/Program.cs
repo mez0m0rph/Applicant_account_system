2510292f0b0525d11f8342942d667325c58911f0
Viktor <a0000@MacBook-Air-0000.local>
2026-04-26 08:11:03 +0700

    DocumentService: добавил обработчик ошибок, обновил program.cs

--- DocumentService/DocumentService.API/Program.cs
+++ DocumentService/DocumentService.API/Program.cs
@@ -1,3 +1,4 @@
+using DocumentService.API.Middleware;
 using DocumentService.Application.Interfaces;
 using DocumentService.Infrastructure.Data;
 using DocumentService.Infrastructure.Repositories;
@@ -45,6 +46,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
